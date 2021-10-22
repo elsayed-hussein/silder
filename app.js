08f0c3219e75ba72ad3efae7de650130b434bbbc
elsayed hussen <79104169+elsayed-hussen@users.noreply.github.com>
2021-10-22 12:47:44 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,99 +1,96 @@
 var sliderImages = Array.from(
   document.querySelectorAll(".slider-container img")
 );
-
 var slidesCount = sliderImages.length;
-
 var currentSlide = 1;
-
 var slideNumberElement = document.getElementById("slide-number");
-
 var nextButton = document.getElementById("next");
 var prevButton = document.getElementById("prev");
-
+var autoButton = document.getElementById("auto");
+var manButton = document.getElementById("man");
 nextButton.onclick = nextSlide;
 prevButton.onclick = prevSlide;
-
 var paginationElement = document.createElement("ul");
-
 paginationElement.setAttribute("id", "pagination-ul");
-
 for (var i = 1; i <= slidesCount; i++) {
   var paginationItem = document.createElement("li");
-
   paginationItem.setAttribute("data-index", i);
-
   paginationItem.appendChild(document.createTextNode(i));
-
   paginationElement.appendChild(paginationItem);
 }
-
 document.getElementById("indicators").appendChild(paginationElement);
-
 var paginationCreatedUl = document.getElementById("pagination-ul");
-
 var paginationsBullets = Array.from(
   document.querySelectorAll("#pagination-ul li")
 );
-
 for (var i = 0; i < paginationsBullets.length; i++) {
   paginationsBullets[i].onclick = function () {
     currentSlide = parseInt(this.getAttribute("data-index"));
-
     theChecker();
   };
 }
-
 theChecker();
+var autoslidebtn;
+autoButton.onclick = autobtn;
+manButton.onclick = manbtn;
 
+function autobtn() {
+  autoslidebtn = setInterval(autoSlide, 1000);
+
+  autoButton.classList.add("hide");
+  manButton.classList.remove("hide");
+}
+function manbtn() {
+  clearInterval(autoslidebtn);
+  manButton.classList.add("hide");
+  autoButton.classList.remove("hide");
+}
+
+function autoSlide() {
+  if (currentSlide == slidesCount) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+    theChecker();
+  }
+}
 function nextSlide() {
   if (nextButton.classList.contains("disabled")) {
     return false;
   } else {
     currentSlide++;
-
     theChecker();
   }
 }
-
 function prevSlide() {
   if (prevButton.classList.contains("disabled")) {
     return false;
   } else {
     currentSlide--;
-
     theChecker();
   }
 }
-
 function theChecker() {
   slideNumberElement.textContent =
     "Slide #" + currentSlide + " of " + slidesCount;
-
   removeAllActive();
-
   sliderImages[currentSlide - 1].classList.add("active");
-
   paginationCreatedUl.children[currentSlide - 1].classList.add("active");
-
   if (currentSlide == 1) {
     prevButton.classList.add("disabled");
   } else {
     prevButton.classList.remove("disabled");
   }
-
   if (currentSlide == slidesCount) {
     nextButton.classList.add("disabled");
   } else {
     nextButton.classList.remove("disabled");
   }
 }
-
 function removeAllActive() {
   sliderImages.forEach(function (img) {
     img.classList.remove("active");
   });
-
   paginationsBullets.forEach(function (bullet) {
     bullet.classList.remove("active");
   });
